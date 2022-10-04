5b579a9fb9718de564a65594fad2b6ec64056606
Eric Bruhns <bruhe002@gmail.com>
2022-10-04 15:58:34 -0700

    Begin implementing errors

--- serverSocket.py
+++ serverSocket.py
@@ -18,27 +18,22 @@ while True:
     print('Ready to serve...')
     connectionSocket, address = serverSocket.accept()
     try:
-        message = connectionSocket.recv(1024)
+        message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = message.decode()
+        outputdata = f.read()
         # Send one HTTP header line into socket
-        # Fill in Start 
-        # Fill in end
-        # 
+        connectionSocket.send("HTTP/1.1 200 OK\r\n" % filename)
+
         # # Send content of the requested file to the client
-        # for i in range(0, len(outputdata)):
-        #   connectionSocket.send(outputdata[i].encode())
-        # connectionSocket.send("\r\n".encode())
-        # 
-        # 
-        # 
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
         connectionSocket.close()
     except IOError:
         pass
         # Send Response message for file not found
-        # Fill in start
-        # Fill in End
+        connectionSocket.send("HTTP/1.1 404 ")
         # Close client socket
         # Fill in start
         # Fill in end
