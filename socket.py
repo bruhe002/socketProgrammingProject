10fedaacb79612025df5136ee878f5659403234b
Eric Bruhns <bruhe002@gmail.com>
2022-10-04 14:04:00 -0700

    Wrote the skeleton code for socket project

---
+++ socket.py
@@ -0,0 +1,45 @@
+from socket import *
+
+import sys
+
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# PREPARE A SERVER SOCKET
+
+# Fill in start
+# Fill in end
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    # connectionSocket, address = Fill in start
+    try:
+        # message = # Fill in start # Fill in end
+        # filename = message.split()[1]
+        # f = open(filename[1:])
+        # outputdata = # Fill in start # Fill in end
+        # Send one HTTP header line into socker
+        # Fill in Start 
+        # Fill in end
+        # 
+        # # Send content of the requested file to the client
+        # for i in range(0, len(outputdata)):
+        #   connectionSocket.send(outputdata[i].encode())
+        # connectionSocket.send("\r\n".encode())
+        # 
+        # 
+        # 
+        # connectionSocket.close()
+        pass
+    except IOError:
+        pass
+        # Send Response message for file not found
+        # Fill in start
+        # Fill in End
+        # Close client socket
+        # Fill in start
+        # Fill in end
+
+    # serverSocket.close()
+    # sys.exit() # Terminate the program after sending the corresponding data 
